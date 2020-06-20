042a52366a6937be99ba2665896f985d722d07d7
Antonio Guglielmo <antonio_guglielmo@hotmail.com>
2020-06-20 15:19:41 -0300

    Add static method Subtraction.of and hide constructor

--- src/main/java/com/acguglielmo/operators/impl/Subtraction.java
+++ src/main/java/com/acguglielmo/operators/impl/Subtraction.java
@@ -5,12 +5,19 @@ import java.util.stream.Collectors;
 
 import com.acguglielmo.operators.Operand;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
+@AllArgsConstructor( access = AccessLevel.PRIVATE )
 public class Subtraction implements Operand<Double, Double> {
 
     private List<Operand<Double, Double>> operands;
+
+    public static Subtraction of( final List<Operand<Double, Double>> operands ) {
+
+    	return new Subtraction(operands);
+
+    }
 
     @Override
     public Double evaluate() {
--- src/test/java/com/acguglielmo/operators/impl/SubtractionTest.java
+++ src/test/java/com/acguglielmo/operators/impl/SubtractionTest.java
@@ -2,7 +2,6 @@ package com.acguglielmo.operators.impl;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
@@ -14,11 +13,10 @@ public class SubtractionTest {
     @Test
     public void testEvaluate() throws Exception {
 
-        final List<Operand<Double, Double>> operands = new ArrayList<>();
-        operands.add( new Value(2.0) );
-        operands.add( new Value(2.0) );
+    	final List<Operand<Double, Double>> operands =
+    		List.of( new Value(2.0), new Value(2.0) );
 
-        final Double result = new Subtraction(operands).evaluate();
+        final Double result = Subtraction.of(operands).evaluate();
 
         assertEquals(0.0, result, 0.01);
 
@@ -27,11 +25,10 @@ public class SubtractionTest {
     @Test
     public void testToString() throws Exception {
 
-        final List<Operand<Double, Double>> operands = new ArrayList<>();
-        operands.add( new Value(2.0) );
-        operands.add( new Value(2.0) );
+    	final List<Operand<Double, Double>> operands =
+        	List.of( new Value(2.0), new Value(2.0) );
 
-        final String result = new Subtraction(operands).toString();
+        final String result = Subtraction.of(operands).toString();
 
         assertEquals("( 2.0 - 2.0 )", result);
 
